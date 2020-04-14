44c86262a99d0e6ba9ff0e379f458d7ac7100a63
selvakumar <selva2242@gmail.com>
2020-04-14 11:46:03 +0530

    Updates

---
+++ precache-manifest.6bf591a3bbecc64f0065167033f18ddc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a858f73ff48d3370edfd6e055bbe802",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "510f41aa5ea4adc58fa0",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c72537d201493d1e93ed",
+    "url": "/portfolio/static/js/2.f18e4b8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.f18e4b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "510f41aa5ea4adc58fa0",
+    "url": "/portfolio/static/js/main.46d7f774.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  }
+]);
